96a1947e5c28f59c5f26a0103700f3d25dc6ec79
ppjamies <pj.jindrich@gmail.com>
2025-09-14 18:05:18 -0700

    moved vpc and subnet cidr blocks into vars.tf as default variables.

--- vars.tf
+++ vars.tf
@@ -1,3 +1,15 @@
+variable "vpc_cidr_block" {
+  type        = string
+  description = "CIDR block for VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr_block" {
+  type        = string
+  description = "CIDR block for VPC"
+  default     = "10.0.1.0/24"
+}
+
 variable "ssh_cidr_blocks" {
   type        = set(string)
   description = "CIDR blocks allowed to SSH"
--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "web-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr_block
 }
 
 resource "aws_subnet" "web-subnet" {
   vpc_id                  = aws_vpc.web-vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = var.subnet_cidr_block
   map_public_ip_on_launch = true
 }
